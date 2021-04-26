b613eb2227df2abb6acffa9e017ec7bd470049e8
Zhihao <zhihao.chen.3@stonybrook.edu>
2021-04-26 03:27:38 -0400

    updates the organization of dataset, updates the experiments

--- mydata.py
+++ mydata.py
@@ -7,8 +7,10 @@ import _pickle as cp
 from datetime import datetime
 import time 
 import matplotlib.pyplot as plt
+import random
 
 NB_SENSOR_CHANNELS = 15
+TRAINING_PERCENTAGE = 0.8
 
 """ def normalization (data,max,min):
     
@@ -25,7 +27,8 @@ def find_dir(dir):
     path_dir = []
     print("not sorted")
     for path in glob.iglob('**/*.csv',recursive=True):
-        if path.find("seg")==-1 and path.find("mobile")==-1 and path.find('sim') == -1 and path.find('test') == -1 and path.find('labels') == -1:
+        if path.find("seg")==-1 and path.find("mobile")==-1 and path.find('sim') == -1  and path.find('labels') == -1 and path.find('sitting')== -1  and path.find('test')== -1 :
+            #if path.find("Back")==-1 and path.find("up")==-1 and path.find("left")==-1 and path.find("down")==-1 and path.find("right")==-1 and path.find("null")==-1:
             path_dir.append(path)
             #print(path)
     
@@ -34,25 +37,68 @@ def find_dir(dir):
     #print(path_dir)
     return path_dir
 
-def processing(p_unique):
+def processing(paths):
     data = pd.DataFrame()
-    for p in p_unique:
+    temp = []
+    for p in paths:
         target = p + "\\sim_dataset.csv"
-        df = pd.read_csv(target,header=None)
-        data = data.append(df)        
-    #print("data:\n",data)
-    #data.to_csv('D:\\test\\project2files\\files\\down\\test.csv',header = False,index = False)
+        if os.path.exists(target):
+            df = pd.read_csv(target,header=None)
+            data = data.append(df)
+        else:
+            temp.append(p)
+
     x = data.iloc[:,1:-1]
     y = data.iloc[:,-1]
     #print(x)
     #print(y)
     data_x = x.to_numpy()
     data_y = y.to_numpy()
-    
+    print("not exists files: \n",temp)
     
     return data_x,data_y
 
-def generate_data(target_filename,p_unique):
+def shuffle_data(state,action,target_filename):
+    
+    training_f = []
+    testing_f =[]
+    NB_SAMPLES = [[]]
+    t1 = 0
+    
+    for s in state:
+        for a in action : 
+            p =  os.path.join(base_path,s,a)
+            t_f =[]
+            totalDir = 0 
+            with os.scandir(p) as entries:
+                for entry in entries:
+                    if entry.is_dir():
+                        totalDir += 1
+                        #print(entry.name)
+                        t_f.append(os.path.join(base_path,s,a,entry.name))
+            #NB_SAMPLES[t1].append(totalDir)
+            #training_f[t1].append(t_f) 
+            BP=int(TRAINING_PERCENTAGE*totalDir)
+            training_f.extend(t_f[:BP])
+            testing_f.extend(t_f[BP:])
+        t1 += 1
+    """ print(testing_f)
+    print(len(testing_f))
+    print(len(training_f)) """
+    random.shuffle(training_f)
+    random.shuffle(testing_f)
+
+    X_train,Y_train = processing(training_f)
+    X_test,Y_test = processing(testing_f)
+    
+    print("Final datasets with size: | train {0} | test {1} | ".format(X_train.shape,X_test.shape))
+
+    obj = [(X_train,Y_train),(X_test, Y_test)]
+    f = open(target_filename, 'wb')
+    cp.dump(obj, f, protocol=-1)
+    f.close()
+
+""" def generate_data(target_filename,p_unique):
     data_x = np.empty((0, NB_SENSOR_CHANNELS))
     data_y = np.empty((0))
     data_x,data_y=processing(p_unique)
@@ -66,14 +112,9 @@ def generate_data(target_filename,p_unique):
     obj = [(X_train,Y_train),(X_test, Y_test)]
     f = open(target_filename, 'wb')
     cp.dump(obj, f, protocol=-1)
-    f.close()
+    f.close() """
 
-def generate_fake_label(lenth):
-    x = list(range(1,lenth))
-    y = list()
-    y = np.random.randint(3,size=lenth)
-    #return pd.Series(y)
-    return y
+
 
 def create_label_series(lenth,label):
     
@@ -85,7 +126,7 @@ def create_labels(p_unique):
     pre_act = []
     #print(p_unique)
     for id in p_unique:
-        print(id)
+        #print(id)
         l=id.split('\\')
         pre_states.append(l[0]) 
         pre_act.append(l[1])
@@ -97,13 +138,13 @@ def create_labels(p_unique):
     size = len(state)*len(action)
     
     labels = np.arange(size).reshape(len(state),len(action))
-    print(labels)
+    #print(labels)
     df = pd.DataFrame(labels,columns=action,index=state)
     print(df) 
     cwd = os.getcwd()
     path = cwd + "/labels.csv"
     df.to_csv(path)
-    return df
+    return df,state,action
 
 def select_label(dataframe,path):
     l=path.split('\\')
@@ -118,7 +159,7 @@ def load_data (files):
         (path,t_filename) = os.path.split(file)
         p.append(path)
     p_unique = list(set(p))
-    label_df = create_labels(p_unique)
+    label_df,state,action = create_labels(p_unique)
     
     for f in files:               
         #df =  pd.read_csv(f,usecols = [2,3,4],header = None)
@@ -132,7 +173,9 @@ def load_data (files):
         #print("after resampling:\n",df)
         max = df.max()
         min = df.min()
+        
         df_norm = (df - min) / (max - min)
+            
         (temp,t_name) = os.path.split(f)
         # 5 kinds of sensors
         if not path == temp:
@@ -140,25 +183,37 @@ def load_data (files):
             count = 1           
             data_path = temp + '\\sim_dataset.csv'
             df_final = df_norm
-            print('_________________new df__________________')
-            print(df_final)
-            print('_______________end new df________________')
             
         else:
             df_final = pd.concat([df_final,df_norm],axis = 1)
             count+=1
             if count == 5:               
-                df_final = df_final.dropna(how='any')                
+                df_final = df_final.dropna(how='any')
+                #print("old shape: ",df_final.shape[0])
+                """ drop_nb =  int(df_final.shape[0] * 0.15)  
+                df_final.drop(df_final.tail(drop_nb).index,inplace=True)
+                df_final.drop(df_final.head(drop_nb).index,inplace=True) """
+                """ plt.cla()
+                df_final.plot() """
+                df_final.drop(df_final.tail(25).index,inplace=True)
+                df_final.drop(df_final.head(5).index,inplace=True)
+                """ df_final.plot()
+                plt.show() """
+                if df_final.empty:
+                    print('DataFrame is empty!')
+                    print(f)
+                    break
+                print("new shape: ",df_final.shape[0])
                 #data_y = generate_fake_label(df_final.shape[0])
                 data_y = create_label_series(df_final.shape[0],label)
-                print(data_y)
+                #print(data_y)
                 df_final['label'] = pd.Series(data_y,index=df_final.index)
                 print(">>>>>>>>>>>>>inserted labels<<<<<<<<<<<<<")
                 print(df_final)                
                 print('---------------drop it ------------------')
-                print(df_final)
+                
                 df_final.to_csv(data_path,header = False)
-    return p_unique
+    return state,action
 
 def resample_f(file):
     #print(file)
@@ -173,21 +228,21 @@ def resample_f(file):
     df[0] = y
     
     #plt.figure()
-    plt.cla()
+    """ plt.cla()
     ax = plt.gca()
-    df.plot(x=0,y=2,style='b*-',label='origin',ax=ax)
+    df.plot(x=0,y=2,style='b*-',label='origin',ax=ax) """
     
     df2 = df.set_index(0).resample('20ms').mean()
     
     df3 = df2.interpolate(method ='linear')
     df = df3 #successfully resampled
      
-    df2.reset_index().plot(x=0,y=2,label = "mean resample",color='Red',marker= 'o',ax = ax)
+    """ df2.reset_index().plot(x=0,y=2,label = "mean resample",color='Red',marker= 'o',ax = ax)
     
     plot_f = df3.plot(y=2,color="Green",label="interpolated",ax = ax)   
     #plt.show()
     fig = plot_f.get_figure()
-    fig.savefig(new_p)  
+    fig.savefig(new_p) """  
     
            
     return df
@@ -197,13 +252,15 @@ def preprocess(dir):
     files = []
     p_unique = []
     files = find_dir(dir)
-    p_unique = load_data(files)
-    target_filename = "C:\\Users\\Zhiha\\Documents\\file_only\\research\\tool_program\\ori\\Sensor-Based-Human-Activity-Recognition-DeepConvLSTM-Pytorch\\data\\processed\\mygestures.data"
-    generate_data(target_filename,p_unique)
+    state,action = load_data(files)
+    target_filename = "C:\\Users\\Zhiha\\Documents\\file_only\\research\\tool_program\\ori\\Sensor-Based-Human-Activity-Recognition-DeepConvLSTM-Pytorch\\data\\processed\\mygestures_ww.data"
+    #generate_data(target_filename,p_unique)
+    shuffle_data(state,action,target_filename)
 
 
 if __name__ == '__main__':
-    base_path = 'D:\\test\\project2files\\2\\files'
+    base_path = 'D:\\test\\project2files\\2\\test\\files\\'
     os.chdir(base_path) #change path
     preprocess(base_path)
     print("done")
+    
